d1b45b701631440f7c91435792cfb4bad07b1fdf
caiohalbert <caio.Halbert@gmail.com>
2024-08-22 21:04:03 -0300

    Adicionar arquivos de projeto.

---
+++ Reapository/EmpregadoRepository.cs
@@ -0,0 +1,39 @@
+﻿using Empresa.Data;
+using Empresa.Models;
+using Empresa.Reapository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Empresa.Reapository
+{
+    public class EmpregadoRepository : IEmpregadoRepository
+    {
+        private readonly dbContext dbContext;
+        public EmpregadoRepository(dbContext dbContext) { 
+            this.dbContext = dbContext;
+        }
+        public Task<Empregado> AddEmpregado(Empregado empregado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmpregado(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empregado> GetEmpregado(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Empregado>> GetEmpregados()
+        {
+            return await dbContext.Empregados.ToListAsync();
+        }
+
+        public Task<Empregado> UpdateEmpregado(Empregado empregado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Reapository/Interface/IDepartamentoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Empresa.Models;
+
+namespace Empresa.Reapository.Interface
+{
+    public interface IDepartamentoRepository
+    {
+        IEnumerable<Departamento> GetDepartamento();
+        Departamento GetDepartamento(int id);
+    }
+}
